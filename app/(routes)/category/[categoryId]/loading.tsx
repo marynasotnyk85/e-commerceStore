7960f6cb87942419594a21eb3e2909d637a31c6c
Maryna <marynasotnyk85@gmail.com>
2024-02-21 09:00:36 +0100

    filters, mobile filter, individual category screen

---
+++ app/(routes)/category/[categoryId]/loading.tsx
@@ -0,0 +1,8 @@
+import Container from "@/components/ui/container";
+//import Skeleton from "@/components/ui/skeleton";
+
+const Loading = () => {
+  return <div>L</div>;
+};
+
+export default Loading;
